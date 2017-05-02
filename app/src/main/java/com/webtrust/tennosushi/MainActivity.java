4526ef677fc5330ecbb713f304d34846ad2ea1be
PantichAlex <alexeypanta@gmail.com>
2017-05-02 16:07:10 +0800

    a

--- app/src/main/java/com/webtrust/tennosushi/MainActivity.java
+++ app/src/main/java/com/webtrust/tennosushi/MainActivity.java
@@ -3,11 +3,14 @@ package com.webtrust.tennosushi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.webtrust.tennosushi.CacheSaver.CacheSaver;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
